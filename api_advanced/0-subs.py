dee12d3723be2883d7b3fceaf6b105c09fdb13fe
waseprisca0 <u.prisca@alustudent.com>
2023-07-15 14:40:41 +0000

    done

---
+++ api_advanced/0-subs.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""get subscribers numbers function"""
+
+
+import json
+import requests
+import sys
+
+
+def number_of_subscribers(subreddit):
+    """get all subscribers"""
+    if len(sys.argv) < 2:
+        return 0
+    else:
+        url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+        headers = {"User-Agent": "Mozilla/5.0"}
+        result = requests.get(url, headers=headers, allow_redirects=False)
+        if result.status_code != 200:
+            return 0
+        body = json.loads(result.text)
+        return body["data"]["subscribers"]

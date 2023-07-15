73d18fe7218bb9aee1f780a2fbbb8e92bcac0350
waseprisca0 <u.prisca@alustudent.com>
2023-07-15 14:43:19 +0000

    done

---
+++ api_advanced/1-top_ten.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+get top 10 hot posts function
+"""
+
+import json
+import requests
+import sys
+
+
+def top_ten(subreddit):
+    """Prints the titles of the first 10 hot posts for a subreddit."""
+    if len(sys.argv) < 2:
+        print(None)
+    else:
+        url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+        headers = {"User-Agent": "Mozilla/5.0"}
+        result = requests.get(url, headers=headers, allow_redirects=False)
+        if result.status_code != 200:
+            print(None)
+        else:
+            data = json.loads(result.text)["data"]["children"]
+            for post in data[:10]:
+                print(post["data"]["title"])
